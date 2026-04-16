75b87d765af7b9d98e0cb85bdca2645a97f94d08
Jhonathan324 <158005754+Jhonathan324@users.noreply.github.com>
2026-04-15 21:53:54 -0300

    Termino do código de geração de chunks
    agora consigo gerar e carrecar as chunks individualmente, sem bugs.

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <conio.h>
 
 #define CHUNK_SIZE 16
 
-typedef short Tile;
 
 typedef struct {
     int x, y; // posição do chunk no mundo
-    Tile tiles[CHUNK_SIZE][CHUNK_SIZE];
+    uint8_t tiles[CHUNK_SIZE][CHUNK_SIZE];
 } Chunk;
 
 // 🔹 Geração procedural simples (substitui depois por Perlin)
@@ -33,7 +34,7 @@ void salvar_chunk(Chunk *c) {
     FILE *f = fopen(nome, "wb");
     if (!f) return;
 
-    fwrite(c->tiles, sizeof(Tile), CHUNK_SIZE * CHUNK_SIZE, f);
+    fwrite(c->tiles, sizeof(uint8_t), CHUNK_SIZE * CHUNK_SIZE, f);
     fclose(f);
 }
 
@@ -48,7 +49,7 @@ void carregar_chunk(Chunk *c, int x, int y) {
     FILE *f = fopen(nome, "rb");
 
     if (f) {
-        fread(c->tiles, sizeof(Tile), CHUNK_SIZE * CHUNK_SIZE, f);
+        fread(c->tiles, sizeof(uint8_t), CHUNK_SIZE * CHUNK_SIZE, f);
         fclose(f);
         printf("Chunk (%d,%d) carregado do disco\n", x, y);
     } else {
@@ -59,19 +60,32 @@ void carregar_chunk(Chunk *c, int x, int y) {
 }
 
 // 🔹 Acessar tile global
-Tile get_tile(int global_x, int global_y) {
-    int chunk_x = global_x / CHUNK_SIZE;
-    int chunk_y = global_y / CHUNK_SIZE;
+uint8_t get_tile(int global_x, int global_y) {
+    int local_x,local_y, chunk_x, chunk_y;
+    if(global_x < 0){
+        chunk_x = (global_x + 1) / CHUNK_SIZE - 1;
+        local_x = CHUNK_SIZE * chunk_x * -1 + global_x;
+    }
+    else{
+       chunk_x = global_x / CHUNK_SIZE;
+       local_x = global_x % CHUNK_SIZE;
+    }
 
-    int local_x = global_x % CHUNK_SIZE;
-    int local_y = global_y % CHUNK_SIZE;
+    if(global_y < 0){
+        chunk_y = (global_y + 1) / CHUNK_SIZE - 1;
+        local_y = CHUNK_SIZE * chunk_y * -1 + global_y;
+    }
+    else{
+       chunk_y = global_y / CHUNK_SIZE;
+       local_y = global_y % CHUNK_SIZE;
+    }
 
     Chunk c;
     carregar_chunk(&c, chunk_x, chunk_y);
 
     for(int i = 0; i < CHUNK_SIZE; i++){
         for(int j = 0; j < CHUNK_SIZE; j++){
-            printf("%2d ", c.tiles[i][j]);
+            printf("%3d ", c.tiles[j][i]);
         }
         printf("\n" );
     }
@@ -81,13 +95,26 @@ Tile get_tile(int global_x, int global_y) {
 }
 
 // 🔹 Modificar tile
-void set_tile(int global_x, int global_y, Tile valor) {
-    int chunk_x = global_x / CHUNK_SIZE;
-    int chunk_y = global_y / CHUNK_SIZE;
+void set_tile(int global_x, int global_y, uint8_t valor) {
+    int local_x,local_y, chunk_x, chunk_y;
+    if(global_x < 0){
+        chunk_x = (global_x + 1) / CHUNK_SIZE - 1;
+        local_x = CHUNK_SIZE * chunk_x * -1 + global_x;
+    }
+    else{
+       chunk_x = global_x / CHUNK_SIZE;
+       local_x = global_x % CHUNK_SIZE;
+    }
 
-    int local_x = global_x % CHUNK_SIZE;
-    int local_y = global_y % CHUNK_SIZE;
-
+    if(global_y < 0){
+        chunk_y = (global_y + 1) / CHUNK_SIZE - 1;
+        local_y = CHUNK_SIZE * chunk_y * -1 + global_y;
+    }
+    else{
+       chunk_y = global_y / CHUNK_SIZE;
+       local_y = global_y % CHUNK_SIZE;
+    }
+    printf("local_x: %d\nlocal_y: %d\n", local_x,local_y);
     Chunk c;
     carregar_chunk(&c, chunk_x, chunk_y);
 
@@ -98,14 +125,43 @@ void set_tile(int global_x, int global_y, Tile valor) {
 
 // 🔹 Teste
 int main() {
-    int x=-10, y=-10;
-    set_tile(x, y, 99);
-    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
+    uint8_t valor = 0;
+    int x=0, y=0;
+    while(valor < 255){
+        int c = _getch();
+        valor++;
+        if (c == 0 || c == 224) { // tecla especial
+            int tecla = _getch();
+            switch (tecla) {
+                case 72:
+                    y--;
+                    set_tile(x, y, valor);
+                    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
+                    break;
+                case 80:
+                    y++;
+                    set_tile(x, y, valor);
+                    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
+                    break;
+                case 75:
+                    x--;
+                    set_tile(x, y, valor);
+                    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
+                    break;
+                case 77:
+                    x++;
+                    set_tile(x, y, valor);
+                    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
+                    break;
+            }
+        }
+        else if (c == 'p'){
+            printf("teste terminado\n");
+            break;
+        }
 
-    x = 11; y = 11;
-    set_tile(x, y, 42);
-    printf("Tile[%d][%d]: %d\n", x, y, get_tile(x, y));
-    set_tile(x, y, 0);
+    }
+
 
     return 0;
 }
